e9b8d63c9a73afcd32c58d98f2c38fcd6e075cb3
victory673 <840894523@qq.com>
2026-02-24 22:37:11 +0800

    hellow

---
+++ bianliang.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int price = 0;
+
+	printf("请输入金额（元）");
+	scanf("%d", &price);
+
+	int change = 100 - price;
+
+	printf("找您%d元\n", change);
+
+	return 0;
+}
